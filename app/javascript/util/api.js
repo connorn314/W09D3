ad452370f25fa87cf2e8333e2d82a965569508a3
Mitchell Jindra <mitchjindra413@gmail.com>
2022-10-05 15:04:39 -0700

    update customfetch

--- app/javascript/util/api.js
+++ app/javascript/util/api.js
@@ -1,10 +1,23 @@
 const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 
 async function customFetch(url, options = {}) {
-  options.headers = {
+  options.headers = { 
     // Your code here
+    "X-CSRF-Token": csrfToken,
     ...options.headers
+    
   };
 
+  // options.headers.append("X-CSRF-Token", csrfToken)
+
+
   return await fetch(url, options);
+}
+
+export const followUser = function(id) {
+  return customFetch(`/users/${id}/follow`, {method: "POST"})
+}
+
+export const unfollowUser = function(id) {
+  return customFetch(`/users/${id}/follow`, { method: "DELETE" })
 }
